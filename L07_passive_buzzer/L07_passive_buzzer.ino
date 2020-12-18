722780c6c011a3249cda3af67310a028dc4af727
Joshua Tharp <joshua.tharp@gmail.com>
2020-12-18 17:26:43 -0600

    [L07] Formatting and spelling changes

--- L07_passive_buzzer/L07_passive_buzzer.ino
+++ L07_passive_buzzer/L07_passive_buzzer.ino
@@ -5,8 +5,7 @@
  
 // notes in the melody:
 int melody[] = {
-    NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6
-  };
+  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
 int duration = 500;  // 500 miliseconds
  
 void setup() {
@@ -15,7 +14,7 @@ void setup() {
  
 void loop() {  
   for (int thisNote = 0; thisNote < 8; thisNote++) {
-    // pin 11 output the voice, every scale is 0.5 second
+    // pin 11 output the voice, every scale is 0.5 sencond
     tone(11, melody[thisNote], duration);
      
     // Output the voice after several minutes
