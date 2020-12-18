1a6a528a27e5c29c8f10e0ae2c82553d76014623
Joshua Tharp <joshua.tharp@gmail.com>
2020-12-18 17:29:59 -0600

    [L08] I used the built-in LED instead of an external one.

--- L08_ball_switch/L08_ball_switch.ino
+++ L08_ball_switch/L08_ball_switch.ino
@@ -1,26 +1,19 @@
-//www.elegoo.com
-//2016.12.08
+// Josh
+// 11/29/2020
 /*****************************************/
-const int ledPin = 13;//the led attach to
+const int pin = 13; // the switch pin
 
-void setup()
-{ 
-  pinMode(ledPin,OUTPUT);//initialize the ledPin as an output
-  pinMode(2,INPUT);
-  digitalWrite(2, HIGH);
+void setup() { 
+  pinMode(LED_BUILTIN, OUTPUT); //initialize the ledPin as an output
+  pinMode(pin, INPUT);
+  digitalWrite(pin, HIGH);
 } 
-/******************************************/
-void loop() 
-{  
-  int digitalVal = digitalRead(2);
-  if(HIGH == digitalVal)
-  {
-    digitalWrite(ledPin,LOW);//turn the led off
-  }
-  else
-  {
-    digitalWrite(ledPin,HIGH);//turn the led on 
+
+void loop()  {  
+  int digitalVal = digitalRead(pin);
+  if(HIGH == digitalVal) {
+    digitalWrite(LED_BUILTIN, LOW); //turn the led off
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH); //turn the led on 
   }
 }
-/**********************************************/
-
