af858df7a98e680d43dda5e802bca7ab1d290fc8
Joshua Tharp <joshua.tharp@gmail.com>
2020-12-26 19:10:16 -0600

    [L12] Renaming the source file and removing the zip library.

--- L12_temp_humidity/L12_temp_humidity.ino
+++ L12_temp_humidity/L12_temp_humidity.ino
@@ -1,34 +1,27 @@
 //www.elegoo.com
 //2018.10.25
 
-
 #include <dht_nonblocking.h>
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 
 static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
-
-
 /*
  * Initialize the serial port.
  */
-void setup( )
-{
+void setup( ) {
   Serial.begin( 9600);
 }
-
-
 
 /*
  * Poll for a measurement, keeping the state machine alive.  Returns
  * true if a measurement is available.
  */
-static bool measure_environment( float *temperature, float *humidity )
-{
+static bool measure_environment( float *temperature, float *humidity ) {
   static unsigned long measurement_timestamp = millis( );
 
-  /* Measure once every four seconds. */
+  /* Measure once every three seconds. */
   if( millis( ) - measurement_timestamp > 3000ul )
   {
     if( dht_sensor.measure( temperature, humidity ) == true )
@@ -41,13 +34,10 @@ static bool measure_environment( float *temperature, float *humidity )
   return( false );
 }
 
-
-
 /*
  * Main program loop.
  */
-void loop( )
-{
+void loop( ) {
   float temperature;
   float humidity;
 
